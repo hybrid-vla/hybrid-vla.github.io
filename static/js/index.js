e05e2c685f8b6cb249dc2584d90c75a013f0a429
hybrid-vla <qigugu0852@sina.com>
2025-03-14 00:18:23 +0800

    Update before release

--- static/js/index.js
+++ static/js/index.js
@@ -77,27 +77,25 @@ $(document).ready(function() {
 
     const videoPaths = {
       franka: {
-        open_drawer: ['default'],
-        pick_and_place: ['add_light', 'blue_block1', 'blue_block2', 'blue_charger1', 'blue_charger2', 'complex_background', 'high_place', 'red_block_grey_bowl1', 'red_block_grey_bowl2', 'strawberry'],
-        pour_water: ['episode1', 'episode2'],
-        pull_charger: ['episode1', 'episode2'],
-        wipe_whiteboard: ['episode1', 'episode2'],
+        open_drawer: 'default',
+        pick_and_place: 'blue_block1',
+        pour_water: 'episode1',
+        pull_charger: 'episode1',
+        wipe_whiteboard: 'episode1',
       },
       agilex: {
-        pick_place: ['episode_1', 'episode_2', 'episode_3'],
-        lift_place: ['episode_1', 'episode_2', 'episode_3', 'episode_4_background', 'episode_5_object', 'episode_6_height', 'episode_7_light'],
-        place_bottle: ['episode_1', 'episode_2', 'episode_3'],
-        wipe_board: ['episode_1', 'episode_2', 'episode_3'],
-        fold_shorts: ['episode_1', 'episode_2', 'episode_3', 'episode_4, episode_5', 'episode_6'],
+        pick_place: 'episode_1',
+        lift_place: 'episode_1',
+        place_bottle: 'episode_1',
+        wipe_board: 'episode_1',
+        fold_shorts: 'episode_1',
       }
     };
 
     $('#robot-select').change(function() {
       const robot = $(this).val();
       const taskSelect = $('#task-select');
-      const episodeSelect = $('#episode-select');
       taskSelect.empty();
-      episodeSelect.empty();
       if (robot) {
         Object.keys(videoPaths[robot]).forEach(task => {
           taskSelect.append(`<option value="${task}">${task}</option>`);
@@ -111,24 +109,9 @@ $(document).ready(function() {
     $('#task-select').change(function() {
       const robot = $('#robot-select').val();
       const task = $(this).val();
-      const episodeSelect = $('#episode-select');
-      episodeSelect.empty();
-      if (task) {
-        videoPaths[robot][task].forEach((episode) => {
-          episodeSelect.append(`<option value="${episode}">${episode}</option>`);
-        });
-        episodeSelect.prop('disabled', false).trigger('change');
-      } else {
-        episodeSelect.prop('disabled', true);
-      }
-    });
-
-    $('#episode-select').change(function() {
-      const robot = $('#robot-select').val();
-      const task = $('#task-select').val();
-      const episode = $(this).val();
       const videoSource = $('#video-source');
-      if (episode) {
+      if (task) {
+        const episode = videoPaths[robot][task];
         videoSource.attr('src', `./static/videos/${robot}/${task}/${episode}.mp4`);
         $('#selected-video')[0].load();
       }
@@ -137,6 +120,4 @@ $(document).ready(function() {
     // Set default selections
     $('#robot-select').val('franka').trigger('change');
     $('#task-select').val('open_drawer').trigger('change');
-    $('#episode-select').val('default').trigger('change');
-
 })
